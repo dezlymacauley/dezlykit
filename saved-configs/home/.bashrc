b404f9f929f638961a10fe650ada2d5cd1bd6119
dezlymacauley <dezlymacauley@proton.me>
2025-08-28 16:37:28 +0200

    Added functionality to load and backup Neovim configuration

--- saved-configs/home/.bashrc
+++
@@ -1,6 +0,0 @@
-alias ls="lsd"
-alias cfgdezlykit="cd $HOME/.config/dezlykit && nvim ."
-
-export PATH="$HOME/.local/bin:$PATH"
-
-eval "$(starship init bash)"
--- scripts/00_load_saved_configs.sh
+++ scripts/00_load_saved_configs.sh
@@ -3,34 +3,49 @@
 #______________________________________________________________________________
 # Neovim
 
-#______________________________________________________________________________
-# Bash
+has_neovim_config=""
 
-# load_save_bashrc() {
-#
-#     local answer=""
-#     local bashrc_from_config_vault="$HOME/.config/dezlykit/saved-configs/home/.bashrc"
-#
-#     while [[ "$answer" != "y" && "$answer" != "n" ]]; do
-#         read -p "Replace current .bashrc with the saved-config from dezlykit? (y / n) " answer
-#
-#         if [[ "$answer" != "y" && "$answer" != "n" ]]; then
-#             echo "Invalid response. Please enter y or n."
-#         fi
-#     done
-#
-#     if [[ "$answer" == "y" ]]; then
-# 	echo
-#         echo "Replacing current .bashrc with the saved-config from dezlykit"
-# 	touch $HOME/.bashrc
-# 	rm $HOME/.bashrc
-# 	cp "$bashrc_from_config_vault" "$HOME/"
-#     else
-#         echo "No changes will be made to your current .bashrc"
-# 	echo
-#     fi
-# }
-#
-# load_save_bashrc
+if [[ ! -d "$HOME/.config/nvim/" ]]; then
+	has_neovim_config="false"
+	echo
+
+	echo "You do not have a Neovim configuration"
+	echo
+
+	echo "Loading the default DezlyKit Neovim config."
+	cp -r "$HOME/.config/dezlykit/saved-configs/default/nvim/" "$HOME/.config/"
+else
+	has_neovim_config="true"
+	echo
+
+	echo "An existing Neovim configuration was found"
+	echo
+
+	perform_backup_of_neovim_config=""
+
+	# Prompt the user until they enter `y` or `n`
+	while [[ "$perform_backup_of_neovim_config" != "y" && "$perform_backup_of_neovim_config" != "n" ]]; do
+	read -p "Would you like to backup your Neovim config? (y / n) " perform_backup_of_neovim_config
+
+         if [[ "$perform_backup_of_neovim_config" != "y" && "$perform_backup_of_neovim_config" != "n" ]]; then
+             echo "Invalid response. Please enter y or n."
+         fi
+
+	done
+
+	# If the user selected yes then replace the default dezlykit config 
+	# with the user's current configuration.
+	
+	if [[ "$perform_backup_of_neovim_config" == "y" ]]; then
+	    # Remove the old default nvim config
+	    rm -rf "$HOME/.config/dezlykit/saved-configs/default/nvim"
+
+	    # Copy the user's current nvim config as the new default
+	    cp -r "$HOME/.config/nvim" "$HOME/.config/dezlykit/saved-configs/default/"
+
+	    echo
+	    echo "Your current Neovim configuration has replaced the default."
+	fi
+fi
 
 #______________________________________________________________________________
