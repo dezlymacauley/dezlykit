cd9cb3a2c9f7d8f0a2856765619855679ad26764
dezlymacauley <dezlymacauley@proton.me>
2025-08-29 09:39:38 +0200

    Removed bashrc function. It is no longer needed

--- upkeep-scripts/03_setup_terminal.sh
+++ upkeep-scripts/03_setup_terminal.sh
@@ -40,37 +40,6 @@ sudo pacman -S --needed yazi
 echo
 
 #______________________________________________________________________________
-# Load .bashrc config from `saved-configs` 
-# or save current config to `saved-configs`
-
-load_save_bashrc() {
-	 
-    local answer=""
-    local bashrc_from_config_vault="$HOME/.config/dezlykit/saved-configs/home/.bashrc"
-
-    while [[ "$answer" != "y" && "$answer" != "n" ]]; do
-        read -p "Replace current .bashrc with the saved-config from dezlykit? (y / n) " answer
-
-        if [[ "$answer" != "y" && "$answer" != "n" ]]; then
-            echo "Invalid response. Please enter y or n."
-        fi
-    done
-
-    if [[ "$answer" == "y" ]]; then
-	echo
-        echo "Replacing current .bashrc with the saved-config from dezlykit"
-	touch $HOME/.bashrc
-	rm $HOME/.bashrc
-	cp "$bashrc_from_config_vault" "$HOME/"
-    else
-        echo "No changes will be made to your current .bashrc"
-	echo
-    fi
-}
-
-load_save_bashrc
-
-#______________________________________________________________________________
 # Install Neovim 
 # I have chosen to build from source so that I am always using the latest
 # version of Neovim.
