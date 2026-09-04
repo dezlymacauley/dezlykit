821f099808c03e4f80720d8393a3d590e65ec182
dezlymacauley <dezlymacauley@gmail.com>
2026-09-04 16:41:05 +0200

    Added ty lsp for Python and conan package manager for C and C++

--- arch-linux/installation-scripts/s01_c_and_cpp_setup.bash
+++ arch-linux/installation-scripts/s01_c_and_cpp_setup.bash
@@ -17,3 +17,22 @@ pacman_install clang
 pacman_install cmake
 
 #______________________________________________________________________________
+
+# Download the vcpkg repo, and run the bootstrap script,
+# or update the repo if it exists
+if [ -d "$HOME/vcpkg" ]; then
+    git -C "$HOME/vcpkg" pull
+else
+    git clone --depth=1 https://github.com/microsoft/vcpkg.git "$HOME/vcpkg"
+    "$HOME/vcpkg/bootstrap-vcpkg.sh"
+fi
+
+#______________________________________________________________________________
+
+# TODO: Consider using conan as a package manager
+
+if command -v uv &>/dev/null; then
+    uv tool install conan
+fi
+
+#______________________________________________________________________________
