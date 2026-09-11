d2355c6ebd1dc0703d4940bb485b74823173bc28
dezlymacauley <dezlymacauley@gmail.com>
2026-09-11 11:02:52 +0200

    Added structure to Neovim config

---
+++ saved-config-files/.config/nvim/init.lua
@@ -0,0 +1,1 @@
+require("d01-core-settings.f05_lsp_settings")
--- saved-config-files/.config/nvim/lua/d01-core-settings/f05_lsp_settings.lua
+++ saved-config-files/.config/nvim/lua/d01-core-settings/f05_lsp_settings.lua
@@ -1,6 +1,6 @@
 --[[
 
-  ABOUT: vim_lsp.lua 
+  ABOUT: f05_lsp_settings.lua
 
 --]]
 
@@ -10,21 +10,24 @@
 
 -- .rs files
 vim.lsp.enable("rust_analyzer")
+-------------------------------------------------------------------------------
 
+-- SECTION: c-lab and cpp-lab
+
+-- .c files and .cpp files
+vim.lsp.enable("clangd")
 -------------------------------------------------------------------------------
 
 -- SECTION: go-lab
 
 -- .go and .gomod files
 vim.lsp.enable("gopls")
-
 -------------------------------------------------------------------------------
 
 -- SECTION: python-lab
 
 -- .py files
 vim.lsp.enable("ty")
-
 -------------------------------------------------------------------------------
 
 -- SECTION: html-and-css-lab
@@ -34,5 +37,18 @@ vim.lsp.enable("html")
 
 -- .css files
 vim.lsp.enable("cssls")
+-------------------------------------------------------------------------------
 
+-- SECTION: typescript-lab
+
+-- .ts and .js files
+vim.lsp.enable("vtsls")
+
+-- .json files
+vim.lsp.enable("jsonls")
+-------------------------------------------------------------------------------
+
+-- SECTION: astro-lab
+
+vim.lsp.enable("astro")
 -------------------------------------------------------------------------------
