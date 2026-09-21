345dd88ec51698c2a92395c13f8c3833c7ffd8d0
dezlymacauley <dezlymacauley@gmail.com>
2026-09-21 11:06:43 +0200

    Added a file tree config

---
+++ saved-config-files/.config/nvim/lua/d02-navigation-plugins/f04_nvim_tree.lua
@@ -0,0 +1,40 @@
+-- ABOUT: Nvim Tree
+
+-- A file tree for Neovim
+
+--=============================================================================
+
+return {
+
+	-- Installation
+	"nvim-tree/nvim-tree.lua",
+	dependencies = {
+		{ "nvim-tree/nvim-web-devicons", opts = {} },
+	},
+	-- Activation and configuration
+	config = function()
+		require("nvim-tree").setup({
+
+			filters = {
+				-- Prevents nvim-tree from hiding
+				-- hidden files that start with a `.`
+				-- E.g. `.sveltekit`
+				dotfiles = false,
+
+				-- Prevents nvim-tree from hiding
+				-- files that are in the `.gitignore` of your project
+				git_ignored = false,
+			},
+			view = {
+				width = 50,
+			},
+			renderer = {
+				icons = {
+					show = {
+						git = false, -- Disables Git status icons (stars, checkmarks, etc.)
+					},
+				},
+			},
+		})
+	end,
+}
